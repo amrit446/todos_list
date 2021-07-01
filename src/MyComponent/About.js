c4a2ca619efeaf84d7b54f5cda3107e122092e2c
amrit446 <hntr776@gmail.com>
2021-07-02 03:27:36 +0530

    Initial commit

---
+++ src/MyComponent/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const About =()=>{
+  return (
+    <div>
+      This is an about component
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, dolores
+        voluptatibus quos sapiente adipisci ullam facere nam quasi rem ducimus
+        mollitia iusto ad!
+      </p>
+    </div>
+  )
+}
